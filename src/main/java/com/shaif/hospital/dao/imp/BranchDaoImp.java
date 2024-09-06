52a3099f7dc77e5b39bffa5d6642f456e4bc29bb
khanshaif9102 <khanshaif9102@gmail.com>
2024-09-06 21:19:04 +0530

    2nd commit

---
+++ src/main/java/com/shaif/hospital/dao/imp/BranchDaoImp.java
@@ -0,0 +1,76 @@
+package com.shaif.hospital.dao.imp;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.shaif.hospital.dao.BranchDao;
+import com.shaif.hospital.dto.Branch;
+import com.shaif.hospital.dto.Hospital;
+
+public class BranchDaoImp implements BranchDao{
+	EntityManagerFactory emf=Persistence.createEntityManagerFactory("shaif");
+	EntityManager em=emf.createEntityManager();
+	EntityTransaction tx=em.getTransaction();
+	
+	@Override
+	public Branch saveBranch(int hid, Branch branch) {
+		Hospital hospital=em.find(Hospital.class, hid);
+		if(hospital!=null) {
+			tx.begin();
+			em.persist(branch);
+			tx.commit();
+			return branch;
+		}
+		return null;
+	}
+
+	@Override
+	public boolean deleteBranch(int bid) {
+		Branch branch=em.find(Branch.class, bid);
+		if(branch!=null) {
+			tx.begin();
+			em.remove(branch);
+			tx.commit();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public Branch getBranchById(int bid) {
+		Branch branch=em.find(Branch.class, bid);
+		if(branch!=null) {
+			tx.begin();
+			tx.commit();
+			return branch;
+		}
+		return null;
+	}
+
+	@Override
+	public Branch updateBranchById(int bid, Branch branch) {
+		Branch branch1=em.find(Branch.class, bid);
+		if(branch1!=null) {
+			tx.begin();
+			branch1.setName(branch.getName());
+			branch1.setAddresses(branch.getAddresses());
+			branch1.setPhno(branch.getPhno());
+			branch1.setEmail(branch.getEmail());
+			em.merge(branch1);
+			tx.commit();
+			return branch1;
+		}
+		return null;
+	}
+
+	@Override
+	public List<Branch> getAllBranches() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/com/shaif/hospital/dao/imp/HospitalDaoImp.java
+++ src/main/java/com/shaif/hospital/dao/imp/HospitalDaoImp.java
@@ -1,5 +1,61 @@
 package com.shaif.hospital.dao.imp;
 
-public class HospitalDaoImp {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
+import com.shaif.hospital.dao.HospitalDao;
+import com.shaif.hospital.dto.Hospital;
+
+public class HospitalDaoImp implements HospitalDao{
+	EntityManagerFactory emf=Persistence.createEntityManagerFactory("pu");
+	EntityManager em=emf.createEntityManager();
+	EntityTransaction tx=em.getTransaction();
+	
+	@Override
+	public Hospital saveHospital(Hospital hospital) {
+		tx.begin();
+		em.persist(hospital);
+		tx.commit();
+		return hospital;
+	}
+
+	@Override
+	public Hospital getHospital(int id) {
+		tx.begin();
+		Hospital hospital=em.find(Hospital.class, id);
+		if(hospital!=null) {
+			tx.commit();
+			return hospital;
+		}
+		return null;
+	}
+
+	@Override
+	public Hospital updateHospital(int hid, Hospital hospital) {
+		tx.begin();
+		Hospital hospital1=em.find(Hospital.class, hid);
+		if(hospital1!=null) {
+			hospital1.setName(hospital.getName());
+			hospital1.setWebsite(hospital.getWebsite());
+			em.merge(hospital1);
+			tx.commit();
+			return hospital1;
+		}
+		return hospital;
+	}
+
+	@Override
+	public boolean deleteHospital(int id) {
+		Hospital hospital=em.find(Hospital.class, id);
+		if(hospital!=null) {
+			tx.begin();
+			em.remove(hospital);
+			tx.commit();
+			return true;
+		}
+		return false;
+	}
+	
 }
